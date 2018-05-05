7593f6aff734b3127f2d42c248d7a1d94ce3679a
Ranger76 <32966218+Ranger76@users.noreply.github.com>
2018-05-05 17:29:54 -0400

    Add files via upload

---
+++ ECE_387_Flex_Module/ECE_387_Flex_Module.ino
@@ -0,0 +1,186 @@
+/*
+ * The script written associated to the flex sensor 
+ * was written by Takdanai Tantiyartyanontha.
+ */
+
+// .writeMicroseconds(1510) makes the 
+// continuous servo 1 stop
+// values above 1510 = ccw
+// values below 1510 = cw
+// .writeMicroseconds(1512) makes the 
+// continuous servo 2 stop
+// values above 1512 = ccw
+// values below 1512 = cw
+// .writeMicroseconds(1490) makes the 
+// continuous servo 3 stop
+// values above 1490 = ccw
+// values below 1490 = cw
+
+#include <Servo.h>;
+
+// Declaring and/or initializing variable to be used 
+// as pin assignments and values written to the 
+// assigned pins.
+int flexPin0 = 0;
+int flexPin1 = 1;
+int flexPin2 = 2;
+int flexPin3 = 3;
+int flexPin4 = 4;
+int flexPin5 = 5;
+int flexRead0;
+int flexRead1;
+int flexRead2;
+int flexRead3;
+int flexRead4;
+int flexRead5;
+int angle_base_0 = 1510;
+int angle_base_1 = 1510;
+int angle_arm = 1510;
+int angle_gripper = 1510;
+Servo base_0; // servo 1
+Servo base_1; // servo 2
+Servo arm; // servo 3
+Servo gripper;
+
+void setup(void)
+{
+  // put your setup code here, to run once:
+  // Setting the baud rate.
+  Serial.begin(9600);
+  
+  // Set up continuous rotation servo pins
+  base_0.attach(6);
+  base_1.attach(9);
+  arm.attach(10);
+  gripper.attach(11);
+}
+
+void loop()
+{
+
+  // put your main code here, to run repeatedly:
+  // Assigning the registered input values from 
+  // pin A0 and A1 to repective variables.
+  flexRead0 = analogRead(flexPin0);
+  flexRead1 = analogRead(flexPin1);
+  flexRead2 = analogRead(flexPin2);
+  flexRead3 = analogRead(flexPin3);
+  flexRead4 = analogRead(flexPin4);
+  flexRead5 = analogRead(flexPin5);
+  
+  // Checking the variables with the input values.
+  Serial.println("flexRead0:");
+  Serial.println(flexRead0);
+  Serial.println("flexRead1:");
+  Serial.println(flexRead1);
+  Serial.println("flexRead2:");
+  Serial.println(flexRead2);
+  Serial.println("flexRead3:");
+  Serial.println(flexRead3);
+  Serial.println("flexRead4:");
+  Serial.println(flexRead4);
+  Serial.println("flexRead5:");
+  Serial.println(flexRead5);
+
+  // testing angle control
+//  angle_base_0 = 1510;
+  //Creates variable for angle_base_0 to be used
+  
+  base_0.writeMicroseconds(angle_base_0);
+  Serial.println("angle_base_0:");
+  Serial.println(angle_base_0);
+  base_1.writeMicroseconds(angle_base_1);
+  Serial.println("angle_base_1:");
+  Serial.println(angle_base_1);
+  arm.writeMicroseconds(angle_arm);
+  Serial.println("anglearm:");
+  Serial.println(angle_arm);
+  
+  // Mapping base_0 motor angle_base_0 
+  // for flexSensor 0.
+  if (flexRead0 >= 400 && flexRead5 >= 350) {
+    angle_base_0 = 1540;
+  }
+  else if (flexRead0 >= 350 && flexRead5 >= 350) {
+    angle_base_0 = 1530;
+  }
+  else if (flexRead0 >= 400 && flexRead5 < 350) {
+    angle_base_0 = 1480;
+  }
+  else if (flexRead0 >= 350 && flexRead5 < 350) {
+    angle_base_0 = 1490;
+  }
+  else if (flexRead0 < 400) {
+    angle_base_0 = 1510;
+  }
+  else {
+  }
+
+  // Mapping base_1 motor angle_base_1 
+  // for flexSensor 1.
+  if (flexRead1 >= 400 && flexRead5 >= 350) {
+    angle_base_1 = 1540;
+  }
+  else if (flexRead1 >= 350 && flexRead5 >= 350) {
+    angle_base_1 = 1530;
+  }
+  else if (flexRead1 >= 400 && flexRead5 < 350) {
+    angle_base_1 = 1480;
+  }
+  else if (flexRead1 >= 350 && flexRead5 < 350) {
+    angle_base_1 = 1490;
+  }
+  else if (flexRead1 < 400) {
+    angle_base_1 = 1512;
+  }
+  else {
+  }
+
+  // Mapping arm motor angle_arm 
+  // for flexSensor 0.
+  if (flexRead3 >= 425 && flexRead5 >= 350) {
+    angle_arm = 1520;
+  }
+  else if (flexRead3 >= 350 && flexRead5 >= 350) {
+    angle_arm = 1510;
+  }
+  else if (flexRead3 >= 425 && flexRead5 < 350) {
+    angle_arm = 1460;
+  }
+  else if (flexRead3 >= 350 && flexRead5 < 350) {
+    angle_arm = 1470;
+  }
+  else if (flexRead3 < 400) {
+    angle_arm = 1490;
+  }
+  else {
+  }
+
+//  // Mapping base_0 motor angle_base_0 
+//  // for flexSensor 0.
+//  if (flexRead0 >= 400 && flexRead5 >= 300) {
+//    angle_base_0 = 1540;
+//  }
+//  else if (flexRead0 >= 350 && flexRead5 >= 300) {
+//    angle_base_0 = 1530;
+//  }
+//  else if (flexRead0 >= 400 && flexRead5 < 300) {
+//    angle_base_0 = 1480;
+//  }
+//  else if (flexRead0 >= 350 && flexRead5 < 300) {
+//    angle_base_0 = 1490;
+//  }
+//  else if (flexRead0 < 400) {
+//    angle_base_0 = 1510;
+//  }
+//  else {
+//  }
+  
+}
+
+
+
+
+
+
+
---
+++ ECE_387__Brushless_Motor_2/ECE_387__Brushless_Motor_2.ino
@@ -0,0 +1,201 @@
+/*
+ * The script written associated to the flex sensor 
+ * was written by Takdanai Tantiyartyanontha.
+ */
+
+ // To vary the speed change the speed (int) varible; 
+ // do not change anything in the setSpeed method.
+
+ // Connect ESC high to pin 6 (PWM) on the Arduino board
+
+#include <Servo.h>;
+#include <SoftwareSerial.h>;
+
+//Minimum/Base Speed and Maximum Speed for motors
+const int baseSpeed = 64;
+const int maxSpeed = 70;
+
+//Pins for Bluetooth
+const int Tx = 2;
+const int Rx = 3;
+const int StatePin = 4;
+
+//Setup Bluetooth Module
+SoftwareSerial btSerial(2,3);
+
+Servo ESC_0;
+Servo ESC_1;
+Servo ESC_2;
+Servo ESC_3;
+
+int connection = 0;
+bool prevConnection = 0;
+char d;
+String data = "";
+
+int speed0 = baseSpeed;
+int speed1 = baseSpeed;
+int speed2 = baseSpeed;
+int speed3 = baseSpeed;
+
+//Checks if a given speed is in range
+int checkSpeed(int i){
+  if(i >= baseSpeed && i <= maxSpeed)
+  return i;
+  else if(i > maxSpeed)
+  return 70;
+  else return 64;
+  
+}
+
+void setup(void)
+{
+  // put your setup code here, to run once:
+  // Setting the baud rate.
+  Serial.begin(9600);
+  btSerial.begin(9600);
+  pinMode(StatePin,INPUT);
+  
+  // Set up ESC pins
+  //ESC_0.attach(6); // Adds ESC;
+  //ESC_1.attach(9); // Adds ESC;
+  //ESC_2.attach(10); // Adds ESC;
+  //ESC_3.attach(11); // Adds ESC;
+  
+}
+
+//Checks for Bluetooth Connection
+ bool connected(){
+  connection = digitalRead(StatePin);
+  if(connection == HIGH && prevConnection == 0){
+    prevConnection = true; 
+    Serial.println("btConnected"); 
+  }else if(connection == LOW && prevConnection == true){
+    prevConnection = false;
+    //When the connection goes off turn off all the motors
+    Serial.println("btDisconnected");
+    ESC_0.detach();
+    ESC_1.detach();  
+    ESC_2.detach();
+    ESC_3.detach();
+    Serial.println("Motors are turned off");
+  }
+  
+ }
+
+bool checkMotorChar(char c){
+  Serial.print("check: " + c);
+  return (int)c >= 'a' && (int)c <= 'd';
+}
+bool checkSpeedChar(char c){
+  Serial.print("check: " + c);
+  return c == '-' && c == '+';
+}
+
+void changeSpeed(String s){
+  char motor = s.charAt(0);
+  char delta = s.charAt(1);
+  switch (motor){
+    case 'a': 
+    if(delta == '+'){
+      Serial.println("ESC0 Speed:: ");
+      Serial.print(speed0);
+      Serial.print(" -> ");
+      Serial.print(checkSpeed(++speed0));
+    //ESC_0.write(checkSpeed(++speed0));
+    }
+    else if (delta == '-'){
+       Serial.println("ESC0 Speed:: ");
+      Serial.print(speed0);
+      Serial.print(" -> ");
+      Serial.print(checkSpeed(--speed0));
+    //ESC_0.write(checkSpeed(--speed0));
+    }
+    break;
+    case 'b': 
+    if(delta == '+'){
+        Serial.println("ESC1 Speed:: ");
+      Serial.print(speed1);
+      Serial.print(" -> ");
+      Serial.print(checkSpeed(++speed1));
+    //ESC_1.write(checkSpeed(++speed1));
+    }
+    else if (delta == '-'){
+       Serial.println("ESC1 Speed:: ");
+      Serial.print(speed1);
+      Serial.print(" -> ");
+      Serial.print(checkSpeed(--speed1));
+    //ESC_1.write(checkSpeed(--speed1));
+    }
+    break;
+    case 'c':
+    if(delta == '+'){
+       Serial.println("ESC2 Speed:: ");
+      Serial.print(speed2);
+      Serial.print(" -> ");
+      Serial.print(checkSpeed(++speed2));
+    //ESC_2.write(checkSpeed(++speed2));
+    }
+    else if (delta == '-'){
+        Serial.println("ESC2 Speed:: ");
+      Serial.print(speed2);
+      Serial.print(" -> ");
+      Serial.print(checkSpeed(--speed2));
+    //ESC_2.write(checkSPeed(--speed2));
+    }
+    break;
+    case 'd':
+    if(delta == '+'){
+         Serial.println("ESC3 Speed:: ");
+      Serial.print(speed3);
+      Serial.print(" -> ");
+      Serial.print(checkSpeed(++speed3));
+    //ESC_3.write(checkSpeed(++speed3));
+    }
+    else if (delta == '-'){
+      Serial.println("ESC3 Speed:: ");
+      Serial.print(speed3);
+      Serial.print(" -> ");
+      Serial.print(checkSpeed(--speed3));
+    //ESC_3.write(checkSpeed(--speed3));
+    }
+    break;
+    default:
+    break;
+}
+}
+
+//Format "a+"
+//first char == motor number
+// second char == speed up or down represented by +/-
+void loop(){
+  connected();
+  
+if(btSerial.available()){
+  d = btSerial.read();
+  Serial.println("d: " + d);
+  data += d;
+  Serial.println("data: " + data);
+  
+  Serial.println(data.length());
+  
+  if(data.length()== 2 && checkMotorChar(data.charAt(0)) && checkSpeedChar(data.charAt(1))){
+    Serial.println("fucku: " + data);
+  changeSpeed(data);
+  data = "";
+  } else if( data.length() == 2){
+    data = "";
+  }
+    }
+  }
+  
+
+
+
+
+
+
+
+
+
+
